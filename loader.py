81e2f7bb6538a9188be76b0ac242fea839d9cec7
idrisovjasur <idrisovjasur@gmail.com>
2025-07-03 21:21:33 +0500

    Middleware

--- loader.py
+++ loader.py
@@ -4,7 +4,9 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.strategy import FSMStrategy
 from aiogram.enums import ParseMode
 from data import config
+from middlewares.throttling import ManualThrottlingMiddleware
 
 bot = Bot(token = config.BOT_TOKEN, default = DefaultBotProperties(parse_mode=ParseMode.HTML))
 storage = MemoryStorage()
 dp = Dispatcher(storage = storage, fsm = FSMStrategy.CHAT, events_isolation = None)
+dp.message.middleware(ManualThrottlingMiddleware())
