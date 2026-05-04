ca00007e0718c91e86d1372873c169a7d8f96dce
idrisovjasur <idrisovjasur@gmail.com>
2026-05-04 11:55:37 +0300

    update-database

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import sys
 
-from loader import dp, bot
+from loader import dp, bot, db
 from utils.set_bot_commands import set_default_commands
 from utils.notify_admins import on_startup_notify
 
@@ -12,6 +12,10 @@ async def main() -> None:
     dp.include_router(start_router)
     await set_default_commands()
     await on_startup_notify()
+    try:
+        db.create_table_users()
+    except Exception as e:
+        print(e)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
--- loader.py
+++ loader.py
@@ -5,8 +5,10 @@ from aiogram.fsm.strategy import FSMStrategy
 from aiogram.enums import ParseMode
 from data import config
 from middlewares.throttling import ManualThrottlingMiddleware
+from utils.db_api.sqlite import Database
 
 bot = Bot(token = config.BOT_TOKEN, default = DefaultBotProperties(parse_mode=ParseMode.HTML))
 storage = MemoryStorage()
 dp = Dispatcher(storage = storage, fsm = FSMStrategy.CHAT, events_isolation = None)
 dp.message.middleware(ManualThrottlingMiddleware())
+db = Database()
